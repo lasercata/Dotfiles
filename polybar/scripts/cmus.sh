e42ccf92cad4e3341192b3816b8b8f79e8341837
lasercata <67599917+lasercata@users.noreply.github.com>
2023-05-09 18:54:36 +0200

    polybar: `cmus.sh`: if no artist, show filename

--- polybar/scripts/cmus.sh
+++ polybar/scripts/cmus.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
+#--------------------------------------------------------------------------------
+#
+# Last modification : 2023.05.09
+# Author            : https://github.com/raytruong/polybar-cmus/, then Lasercata
+# Version           : v1.1
+#
+#--------------------------------------------------------------------------------
+
 prepend_zero () {
         seq -f "%02g" $1 $1
 }
 
 artist=$(echo -n $(cmus-remote -C status | grep "tag artist" | cut -c 12-))
+song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
+fn=$(basename "$(cmus-remote -Q | grep file)" .mp3)
 
-if [[ $artist = *[!\ ]* ]]; then
-        song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
-        position=$(cmus-remote -C status | grep position | cut -c 10-)
-        minutes1=$(prepend_zero $(($position / 60)))
-        seconds1=$(prepend_zero $(($position % 60)))
-        duration=$(cmus-remote -C status | grep duration | cut -c 10-)
-        minutes2=$(prepend_zero $(($duration / 60)))
-        seconds2=$(prepend_zero $(($duration % 60)))
-        echo -n "$artist - $song [$minutes1:$seconds1/$minutes2:$seconds2]"
+if [[ $artist = *[!\ ]* ]] && [[ $song = *[!\ ]* ]]; then
+    position=$(cmus-remote -C status | grep position | cut -c 10-)
+    minutes1=$(prepend_zero $(($position / 60)))
+    seconds1=$(prepend_zero $(($position % 60)))
+    duration=$(cmus-remote -C status | grep duration | cut -c 10-)
+    minutes2=$(prepend_zero $(($duration / 60)))
+    seconds2=$(prepend_zero $(($duration % 60)))
+    echo -n "$artist - $song [$minutes1:$seconds1/$minutes2:$seconds2]"
+
+elif [[ $fn = *[!\ ]* ]]; then
+    position=$(cmus-remote -C status | grep position | cut -c 10-)
+    minutes1=$(prepend_zero $(($position / 60)))
+    seconds1=$(prepend_zero $(($position % 60)))
+    duration=$(cmus-remote -C status | grep duration | cut -c 10-)
+    minutes2=$(prepend_zero $(($duration / 60)))
+    seconds2=$(prepend_zero $(($duration % 60)))
+    echo -n "$fn [$minutes1:$seconds1/$minutes2:$seconds2]"
+
 else
-        echo
+    echo
 fi
