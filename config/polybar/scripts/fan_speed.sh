29d5ac38079e6193044a46f01a72e6411444478c
lasercata <67599917+lasercata@users.noreply.github.com>
2023-05-29 15:20:06 +0200

    polybar: adding fan stat

---
+++ config/polybar/scripts/fan_speed.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#--------------------------------
+#
+# Last modification : 2023.05.29
+# Author            : Lasercata
+# Version           : v1.0
+#
+#--------------------------------
+
+# Inspired from https://github.com/polybar/polybar-scripts/blob/master/polybar-scripts/system-fan-speed/system-fan-speed.sh
+
+
+speed=$(sensors | grep fan | awk '{print $2}')
+
+if [ "$speed" != "" ]; then
+    echo "$speed/100" | bc -l | LANG=C xargs printf "%.0f\n"
+    # echo $speed rpm
+else
+    echo ""
+fi
