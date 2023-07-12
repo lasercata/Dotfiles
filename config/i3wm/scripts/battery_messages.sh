3c08bbfc4ff43771af9f5ca5de2fadceeb23b522
lasercata <67599917+lasercata@users.noreply.github.com>
2023-07-12 21:45:36 +0200

    i3: correcting `battery_messages.sh`
    Should work now (not tested with low battery tho)

--- config/i3wm/scripts/battery_messages.sh
+++ config/i3wm/scripts/battery_messages.sh
@@ -4,10 +4,15 @@
 #
 # Last update : 2023.07.12
 # Author      : lasercata
+<<<<<<< Updated upstream
 # Version     : v1.1
+=======
+# Version     : v1.2.0
+>>>>>>> Stashed changes
 #
 #--------------------------
 
+##-Init
 CRITICAL=5
 DANGER=10
 WARN=15
@@ -15,7 +20,7 @@ change_time=20
 
 SLEEP_TIME=5 #minutes
 
-
+##-Function that show notification after checking battery level
 function check_power {
     state=$(upower -i $(upower -e | grep 'BAT') | grep 'state' | awk '{print $2}')
     percentage=$(upower -i $(upower -e | grep 'BAT') | grep 'percentage' | awk '{print $2}' | sed s/%//)
@@ -23,6 +28,7 @@ function check_power {
     echo "state: $state"
     echo "percentage: $percentage%"
 
+<<<<<<< Updated upstream
     echo "critical: $CRITICAL%"
 
     # if (( $percent<=$CRITICAL )); then
@@ -47,6 +53,31 @@ function check_power {
             SLEEP_TIME=1;
 
         elif (( $percent<=$change_time )); then
+=======
+    if [[ "$state" == "discharging" ]]; then
+        if (( $percentage <= $CRITICAL )); then
+            i3lock -t -c 000000 -i ~/.wallpapers/lasercata_logo_fly_on_parrot_bk_modif_9.png &&
+            systemctl suspend #lock screen and fell asleep
+
+            notify-send "Critical battery !" "Current battery level : $percentage%"
+            echo "Critical battery ! Current battery level : $percentage%"
+
+            SLEEP_TIME=1;
+
+        elif (( $percentage <= $DANGER )); then #same.
+            notify-send "Low battery !" "Current battery level : $percentage%"
+            echo "Low battery ! Current battery level : $percentage%"
+
+            SLEEP_TIME=1;
+
+        elif (( $percentage <= $WARN )); then #same.
+            notify-send "Low battery" "Current battery level : $percentage%";
+            echo "Low battery ! Current battery level : $percentage%"
+
+            SLEEP_TIME=1;
+
+        elif (( $percentage <= $change_time )); then
+>>>>>>> Stashed changes
             SLEEP_TIME=1;
 
         fi
@@ -56,9 +87,9 @@ function check_power {
     fi
 }
 
+##-Main loop
 while [[ true ]]; do
     check_power
     sleep "$SLEEP_TIME"m
 done
 
-#TODO: test this script ...
