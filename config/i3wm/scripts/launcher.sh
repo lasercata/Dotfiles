c3f2198db5b3bc5c24c61c3c71d94197a48d703d
lasercata <67599917+lasercata@users.noreply.github.com>
2023-08-01 21:59:30 +0200

    i3: update `launcher.sh` and use it for `udiskie`.

--- config/i3wm/scripts/launcher.sh
+++ config/i3wm/scripts/launcher.sh
@@ -2,13 +2,14 @@
 
 #--------------------------
 #
-# Last update : 2023.07.14
+# Last update : 2023.08.01
 # Author      : lasercata
-# Version     : v1.0.0
+# Version     : v1.0.1
 #
 #--------------------------
 
 # This script kill the program given in argument, and relaunch it after.
+# This is usefull to have only one instace of the program running after restarting i3 in place.
 
-killall $(basename "$1")
+killall $(basename "$1" | awk '{print $1}')
 $1
