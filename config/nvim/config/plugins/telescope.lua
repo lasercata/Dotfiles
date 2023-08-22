9fd75a0b93dc1b1987b8a3c2faa496f3caba8689
lasercata <67599917+lasercata@users.noreply.github.com>
2023-08-22 22:02:35 +0200

    nvim: adding bindings (telescope and which-key)
    leader+f is now for old_files ;
    adding leader+/ to fuzzy search ;
    adding leader+w to show all mappings.

--- config/nvim/config/plugins/telescope.lua
+++ config/nvim/config/plugins/telescope.lua
@@ -1,8 +1,23 @@
+--=== Bindings
+vim.keymap.set(
+    'n',
+    '<leader>F',
+    '<cmd>Telescope find_files<cr>',
+    {desc = 'Open telescope for files'}
+)
+
 vim.keymap.set(
     'n',
     '<leader>f',
-    '<cmd>Telescope find_files<cr>',
-    {desc = 'Open telescope for files'}
+'<cmd>Telescope oldfiles<cr>',
+    {desc = 'Open telescope for recent files'}
+)
+
+vim.keymap.set(
+    'n',
+    '<leader>/',
+    '<cmd>Telescope current_buffer_fuzzy_find<cr>',
+    {desc = 'Fuzzy find (telescope)'}
 )
 
 vim.keymap.set(
--- config/nvim/config/plugins/which-key.lua
+++ config/nvim/config/plugins/which-key.lua
@@ -1,7 +1,16 @@
+--=== Setup
 require('which-key').setup(
     {
         window = {
             winblend = 20
         }
     }
+)
+
+--=== Bindings
+vim.keymap.set(
+    'n',
+    '<leader>w',
+    '<cmd>WhichKey<cr>',
+    {desc = 'Show all mappings'}
 )
