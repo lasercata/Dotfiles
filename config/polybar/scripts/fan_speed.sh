e5423e5a6231737da205bc456b04457531fc1ed0
lasercata <67599917+lasercata@users.noreply.github.com>
2023-12-30 15:55:24 +0100

    polybar: adding visual ram warning

--- config/polybar/scripts/fan_speed.sh
+++ config/polybar/scripts/fan_speed.sh
@@ -2,16 +2,16 @@
 
 #--------------------------------
 #
-# Last modification : 2023.05.29
+# Last modification : 2023.12.17
 # Author            : Lasercata
-# Version           : v1.0
+# Version           : v1.1
 #
 #--------------------------------
 
 # Inspired from https://github.com/polybar/polybar-scripts/blob/master/polybar-scripts/system-fan-speed/system-fan-speed.sh
 
 
-speed=$(sensors | grep fan | awk '{print $2}')
+speed=$(sensors | grep cpu_fan | awk '{print $2}')
 
 if [ "$speed" != "" ]; then
     echo "$speed/100" | bc -l | LANG=C xargs printf "%.0f\n"
