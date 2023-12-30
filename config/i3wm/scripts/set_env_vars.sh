db3c91ca944ac07687436d92d5d5d9cadb14d5d0
lasercata <67599917+lasercata@users.noreply.github.com>
2023-12-30 13:04:52 +0100

    i3: corrections (env vars, ...)
    - Adding a script to set environnement variables (used with rofi) ;
    - Using `--no-startup-id` when needed ;
    - Adding a `Window rules` section.

---
+++ config/i3wm/scripts/set_env_vars.sh
@@ -0,0 +1,15 @@
+#--------------------------------
+#
+# Last modification : 2023.12.30
+# Author            : Lasercata
+# Version           : v1.0.0
+#
+#--------------------------------
+
+# This script is called before running rofi (cf i3 config file) in order to have the variables set.
+
+#------Integrate KDE apps in i3
+export QT_QPA_PLATFORMTHEME="qt5ct"
+
+#------Gnome dark mode
+export GTK_THEME=Adwaita:dark
