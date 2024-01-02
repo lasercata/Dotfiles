fc65aca7aa4a3af920678a1616ee06724f426ae6
lasercata <67599917+lasercata@users.noreply.github.com>
2024-01-02 19:22:27 +0100

    polybar: adding a useless bluetooth script
    That is not used obviously

--- config/polybar/scripts/bluetooth.sh
+++ config/polybar/scripts/bluetooth.sh
@@ -2,12 +2,12 @@
 
 # From https://github.com/msaitz/polybar-bluetooth
 
-if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
+if [ "$(bluetoothctl show | grep "Powered: yes" | wc -c)" -eq 0 ]
 then
   echo "%{F#66ffffff}󰂲" #off
 else
-  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
-  then 
+  if [ "$(echo info | bluetoothctl | grep 'Device' | wc -c)" -eq 0 ]
+  then
       echo "%{F#f6668a}%{u#f6668a}%{+u}" #active but not connected (red-3)
   fi
   echo "%{F#2193ff}%{u#2193ff}%{+u}" #connected
