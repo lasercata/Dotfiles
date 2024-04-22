084a4162e615cbb4abeed7fa2245a7e1404c337e
lasercata <67599917+lasercata@users.noreply.github.com>
2024-04-22 22:19:48 +0200

    Correction in touchpad toggle script

--- config/i3wm/scripts/toggle_touchpad_libinput.sh
+++ config/i3wm/scripts/toggle_touchpad_libinput.sh
@@ -8,14 +8,14 @@
 #
 #---------------------------------
 
-id=$(xinput list | grep -i touchpad | awk '{ print $5 }' | awk -F = '{print $2}')
+id=$(xinput list | grep -i touchpad | awk -F '\t' '{ print $2 }' | awk -F = '{print $2}')
 
 enabled=$(xinput list-props $id | grep "Device Enabled" | awk '{print $4}')
 
-if [ $enabled = 1 ]; then
+if [[ $enabled -eq 1 ]]; then
     xinput set-prop $id "Device Enabled" 0;
 
-elif [ $enabled = 0 ]; then
+elif [[ $enabled -eq 0 ]]; then
     xinput set-prop $id "Device Enabled" 1;
 
 else
