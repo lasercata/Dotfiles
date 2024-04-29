c05e3264ce585f3195c8ac76c3db3db27e9679fa
lasercata <67599917+lasercata@users.noreply.github.com>
2024-04-29 16:32:07 +0200

    i3: touchpad script: change message

--- config/i3wm/scripts/toggle_touchpad_libinput.sh
+++ config/i3wm/scripts/toggle_touchpad_libinput.sh
@@ -2,9 +2,9 @@
 
 #---------------------------------
 #
-# Last modification : 2023.05.28
+# Last modification : 2024.04.29
 # Author            : Lasercata
-# Version           : v1.0
+# Version           : v1.0.1
 #
 #---------------------------------
 
@@ -14,11 +14,11 @@ enabled=$(xinput list-props $id | grep "Device Enabled" | awk '{print $4}')
 
 if [[ $enabled -eq 1 ]]; then
     xinput set-prop $id "Device Enabled" 0;
-    notify-send "Touchpad toggling" "Touchpad turned off";
+    notify-send "Touchpad off" "Touchpad turned off";
 
 elif [[ $enabled -eq 0 ]]; then
     xinput set-prop $id "Device Enabled" 1;
-    notify-send "Touchpad toggling" "Touchpad turned on";
+    notify-send "Touchpad on" "Touchpad turned on";
 
 else
     exit 1;
