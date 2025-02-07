3afa3c7d0166175ce91f3ef5633a3886fb8352ad
lasercata <67599917+lasercata@users.noreply.github.com>
2025-02-07 17:48:49 +0100

    nvim: neoscroll: change easing function

--- config/nvim/config/plugins/neoscroll.lua
+++ config/nvim/config/plugins/neoscroll.lua
@@ -1,1 +1,9 @@
-require('neoscroll').setup()
+require('neoscroll').setup({
+    easing = 'quadratic',
+    -- duration_multiplier = 1.0,
+    mappings = {
+        '<C-u>', '<C-d>',
+        '<C-b>', '<C-f>',
+        'zt', 'zz', 'zb',
+    },
+})
