54601024710785f468f265fab34a6314ba5242f7
lasercata <contact@lasercata.com>
2026-01-30 11:51:42 +0100

    polybar: hot pluggable screens: switch left and right

--- config/polybar/scripts/hot_pluggable_screens.sh
+++ config/polybar/scripts/hot_pluggable_screens.sh
@@ -10,18 +10,19 @@ done
 
 #---Placing the connected monitors.
 # The main is at the centre, and then each monitor is added alternatively to the right or the left.
-# E.g for monitors 1 2 3 4 5, with 1 the main, the placement will be 5 3 1 2 4.
+# E.g for monitors 1 2 3 4 5, with 1 the main, the placement will be 5 3 1 2 4 (right and then left, and loop).
+# Note 2026.01.30: switching the order (left then right, loop)
 left_mon=$main_mon
 right_mon=$main_mon
 i=0
 
 for m in $(xrandr --query | grep " connected" | cut -d' ' -f1); do
     if [[ $m != $main_mon ]]; then # Ignore the main monitor
-        if [ $((i % 2)) -eq 0 ]; then # Add to right
-            xrandr --output $m --auto --right-of $right_mon
-            right_mon=$m
-        else # Add to left
+        if [ $((i % 2)) -eq 0 ]; then # Add to left
             xrandr --output $m --auto --left-of $left_mon
+            right_mon=$m
+        else # Add to right
+            xrandr --output $m --auto --right-of $right_mon
             left_mon=$m
         fi
 
